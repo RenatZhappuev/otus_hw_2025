7eb0b0e9b9f2044bd571cf021940c3b5a19bad6e
RenatZhappuev <RenatZhappuev@yandex.ru>
2025-04-03 19:56:25 +0300

    chessboard_hw

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello Otus!")
+	fmt.Println(createChessboard(8, 8))
 }
+
+func createChessboard(lenn int, wid int) string {
+	var chessboard strings.Builder
+	for i := 0; i < lenn; i++{
+		for j := 0; j < wid; j++{
+			if (i+j) % 2 == 0{
+				chessboard.WriteString(" ")
+			} else {
+				chessboard.WriteString("#")
+			}
+		}
+		chessboard.WriteString("\n")
+	}
+	return chessboard.String()
+}
+
